a803ec99be2241eba8a868d9563d21edf6ece3e3
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-07-10 22:04:36 -0300

    feat: add AgencyNotFoundOrNotActiveExcetion

---
+++ src/main/java/br/com/bank/exceptions/AgencyNotFoundOrNotActiveExcetion.java
@@ -0,0 +1,7 @@
+package br.com.bank.exceptions;
+
+public class AgencyNotFoundOrNotActiveExcetion extends RuntimeException{
+    public AgencyNotFoundOrNotActiveExcetion(){
+        super();
+    }
+}
